1b3fd87b4eaeb3d3f99f809d51f8c73a52381061
Kamaliki Clive <justuskamaliki@gmail.com>
2023-01-19 17:30:47 +0300

    "a failing test"

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { act } from "react-dom/test-utils";
+
+import Appointment from "../src/Appointment";
+
 describe ("Appointment", () => {
     it("renders the customer first name", () => {
+        const customer = {firstName: "Ash"};
+        
+        const component = <Appointment customer={customer} />;
+        const container = document.createElement("div");
+        document.body.appendChild(container);
+        act (() => {
+            ReactDOM.createRoot(container).render(component);
+        });
         expect(document.body.textContent).toMatch("Ash");
     });
 });
